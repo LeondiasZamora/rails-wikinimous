bc9ea22bb470701e22840163816cfee073fb5405
Leonidas Zamora Gottwald <leonidas.zamora.gottwald@gmail.com>
2024-11-12 16:15:39 +0100

    finished with the logic. could use some css bootstrap still

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,12 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+require 'faker'
+
+10.times do
+  article = Article.create(
+    title: Faker::Sports::Football.team,
+    content: Faker::Sports::Football.player
+  )
+  article.save!
+end
